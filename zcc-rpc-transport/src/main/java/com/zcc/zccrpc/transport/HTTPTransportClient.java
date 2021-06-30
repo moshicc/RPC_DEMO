88ee689a6133c87cb0b047674be5ff645d3999ff
“p52_zcc” <1316435855@qq.com>
2021-06-30 23:19:15 +0800

    实现HTTPTransportClient 和HTTPTransportServer

--- zcc-rpc-transport/src/main/java/com/zcc/zccrpc/transport/HTTPTransportClient.java
+++ zcc-rpc-transport/src/main/java/com/zcc/zccrpc/transport/HTTPTransportClient.java
@@ -1,21 +1,48 @@
 package com.zcc.zccrpc.transport;
 
 import com.zcc.zccrpc.Peer;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * 实现
  */
 public class HTTPTransportClient implements TransportClient {
+    private String url;
+
     @Override
     public void connect(Peer peer) {
-
+        this.url = "http://" + peer.getHost()+":"+peer.getPort();
     }
 
     @Override
     public InputStream write(InputStream data) {
-        return null;
+        try{
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            connection.setRequestMethod("POST");
+
+            connection.connect();
+            //把data数据发送给server
+            IOUtils.copy(data, connection.getOutputStream());
+
+            int resultCode = connection.getResponseCode();
+            if(resultCode == connection.HTTP_OK) {
+                return connection.getInputStream();
+            } else {
+                return connection.getErrorStream();
+            }
+
+        }catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @Override
